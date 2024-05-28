3f9d5e6ff928774cea1e8ff5ed2051c057b32504
Geovane Borba <geovanne@live.com>
2024-05-28 09:14:24 -0300

    feat: implement user registration controller

---
+++ src/http/controller/register.ts
@@ -0,0 +1,30 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { RegisterUseCase } from '@/use-cases/register'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+
+const registerBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
+})
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    await registerUseCase.execute({ name, email, password })
+
+    return reply.status(201).send('User registered successfully')
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    throw error
+  }
+}
