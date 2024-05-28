f53edbb3e3f33370efa8d07ee3647b0097028cd4
Geovane Borba <geovanne@live.com>
2024-05-28 16:08:32 -0300

    fix: UserAlreadyExistsError import

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,4 @@
-import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { UsersRepository } from '@/repositories/users-repository'
 import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
