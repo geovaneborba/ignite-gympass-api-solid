bda267aa2d2525cb9b226d0e4e610ba8ff1a8760
Geovane Borba <geovanne@live.com>
2024-05-28 09:11:11 -0300

    feat: implement UsersRepository interface in InMemoryUsersRepository

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,36 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { Prisma, User } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = []
+
+  async findById(id: string): Promise<User | null> {
+    const user = this.users.find((item) => item.id === id)
+
+    return user || null
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.users.find((user) => user.email === email)
+
+    console.log('findByEmail:', email, 'result:', user)
+
+    return user || null
+  }
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user: User = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+      role: 'MEMBER',
+    }
+
+    this.users.push(user)
+
+    return user
+  }
+}
